8105853513df259503e5a5091c3ee499b1d7f4d3
Space L Clottey <31475561+spaceclottey@users.noreply.github.com>
2024-02-12 08:55:45 -0500

    Add files via upload

--- filmdata.js
+++ filmdata.js
@@ -1,45 +1,64 @@
-let filmData = {
-  "Spotting Aaron": {
+let filmData = [
+  {
     title: "Spotting Aaron",
-    workedOn: "",
-    url: "SpottingAaron.mp4",
+    workedOn: "Nov 2023",
+    url: "https://www.youtube.com/embed/L8S1OmaDN5s?si=_7RX5mRWjUR8YevV",
     description: "Aaron tries to take a test.",
   },
-  "Trailer Mashup": {
+  {
     title: "Trailer Mashup",
     workedOn: "Oct 2023",
-    url: "Trailer_Mashup.mp4",
-    description: "",
+    url: "https://www.youtube.com/embed/3_Kq0XcKOC8?si=K7PjH16No8qQtySb",
+    description: "Everything was made using the power of love. Nobody really knows what it was caused by, but who cares?",
 
   },
-  "Painting the Portrait": {
+  {
     title: "Painting the Portrait",
-    workedOn: "Long time ago",
-    url: "https://www.youtube.com/watch?v=QArHnmJhCMs",
+    workedOn: "Sum 2022",
+    url: "https://www.youtube.com/embed/QArHnmJhCMs?si=H-T3y0u6SpDOE2Q4",
     description: "All in one take.",
   },
-  "Temp": {
-    title: "",
-    workedOn: "",
-    url: "",
-    description: "",
+  {
+    title: "The Entire History of the Endverse",
+    workedOn: "Oct 2023",
+    url: "https://www.youtube.com/embed/HnvrvJ22z7M?si=foL6afip5DVphw_p",
+    description: "Everything that happens in the entire Ender's Game book series.",
+  },
+  {
+    title: "Misaligned",
+    workedOn: "Jul 2022",
+    url: "https://www.youtube.com/embed/9ocGLIK8ib8?si=H5oQS_YoysI7MWxz",
+    description: "Two children at an Artificial Intelligence Alignment Camp fall in love, and everything is amazing. Or is it?"
   }
-}
+]
 
-let filmDivs = [];
-for (const [key, film] of Object.entries (filmData)) {
-  filmDivs.push(
-    `<div class='filmcard'>
-    <h2> ${film.title} </h2>
-    <iframe width="420" height="230" src="${film.url}"/>
-    <p> ${film.workedOn} </p>
-    <p> ${film.description} </p>
-  </div>
-  `
-  )
-}
+document.addEventListener ('DOMContentLoaded', () => {
+  const filmcardsContainer = document.querySelector ('.filmcardsContainer');
 
-let filmContainer = document.getElementById("filmcardsContainer")
-filmDivs.forEach(filmDiv => {
-  filmContainer.innerHTML += filmDiv;
-})
+  filmData.forEach (film => {
+    const filmCard = document.createElement ('div');
+    filmCard.classList.add ('filmcard');
+
+    const iframe = document.createElement ('iframe');
+    iframe.src = film.url;
+    iframe.frameBorder = '0';
+    iframe.allow =
+      'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+    iframe.allowFullscreen = true;
+    filmCard.appendChild (iframe);
+
+    const title = document.createElement ('h2');
+    title.textContent = film.title;
+    filmCard.appendChild (title);
+
+    const workedOn = document.createElement ('p');
+    workedOn.textContent = film.workedOn;
+    filmCard.appendChild (workedOn);
+
+    const description = document.createElement ('p');
+    description.textContent = film.description;
+    filmCard.appendChild (description);
+
+    filmcardsContainer.appendChild (filmCard);
+  });
+});
